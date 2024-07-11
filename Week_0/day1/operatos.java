dcc205cea571b55743d25441f8ae26c25f6595c9
unknown <namanfuloria14@gmail.com>
2024-07-11 14:05:26 +0530

    Day1

---
+++ Week_0/day1/operatos.java
@@ -0,0 +1,41 @@
+package Week_0.day1;
+
+public class operatos {
+    public static void main(String[] args) {
+      /*   int a=6,b=4;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+        //arithmetic operator
+        int c=2;
+       System.out.println(c++);
+System.out.println(++c);
+int x=2;
+x+=3;//x=x+3
+x-=3; //x=x-3
+x*=3; //x=x*3
+x/=3;// x=x/3
+x%=3; //x=x%3
+
+int sum=0;
+for(int i=1;i<=25;i++){
+    sum=sum+i;
+    System.out.print(sum +" , ");
+}
+/*
+ * int i=5;
+ * while (i<15){
+ * code
+ * i++
+ * }
+ */
+//output the first multiplw of 2&7
+int k=5;
+while(k<15){
+    if(k%2==0 && k%7==0){
+System.out.println(k);
+    }
+    k++;
+}
+
+}
+}
